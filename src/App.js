382f66150a3e69e0c51f8c755f70f4e4874642b8
Baruch <baruch14390@gmail.com>
2019-12-30 20:40:19 +0200

    react file

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Link to='/'>HomePage</Link>
+        <Link to='/page2'>2</Link>
+
+        <Switch>
+          <Route
+            exact
+            path='/'
+            component={() => {
+              return <h1>HomePage</h1>;
+            }}
+          />
+          <Route
+            exact
+            path='/page2'
+            component={() => {
+              return <h1>Page2</h1>;
+            }}
+          />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
