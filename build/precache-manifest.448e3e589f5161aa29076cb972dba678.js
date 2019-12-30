c9f17c75b5593f03ea8e2886b411298ea2ead2c6
Baruch <baruch14390@gmail.com>
2019-12-30 21:08:56 +0200

    testing react

---
+++ build/precache-manifest.448e3e589f5161aa29076cb972dba678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abdfcfb548021f20fb1d409e66b6b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "443902b56e6b709d1e44",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "082da279c75ca7cf280b",
+    "url": "/static/js/2.f12ca650.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f12ca650.chunk.js.LICENSE"
+  },
+  {
+    "revision": "443902b56e6b709d1e44",
+    "url": "/static/js/main.70cc64d7.chunk.js"
+  },
+  {
+    "revision": "d57df049333bcb5c1737",
+    "url": "/static/js/runtime-main.2449dd0f.js"
+  }
+]);
